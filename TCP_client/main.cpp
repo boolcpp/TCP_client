a7176aa8033f283684752b5434e324a31d5444c7
Fedor Zernov <fedor11487@mail.ru>
2018-04-21 03:08:35 +0300

    buffers of rowsCount/colsCount/Length

--- TCP_client/main.cpp
+++ TCP_client/main.cpp
@@ -6,51 +6,57 @@
 //for video
 #include <opencv2/opencv.hpp>
 
-
+//#include <stdlib.h>
 int main()
 {
 	std::cout << "start" << std::endl;
-	cv::Mat img0 = cv::imread("1.jpg");
+	cv::Mat img0 = cv::imread("12.jpg");
 	//mat to char
-	unsigned char* ptrbuf = new unsigned char[img0.cols * img0.rows * 3 + 8];
-	
-	
-	char rowbuf[4];// = new unsigned char[4];
-	ZeroMemory(rowbuf, 4);
+	char* ptrbuf = new char[img0.cols * img0.rows * 3 + 26];
+
+
+	char rowbuf[5];// = new unsigned char[4];
+	ZeroMemory(rowbuf, 5);
 	sprintf_s(rowbuf, "%d", img0.rows);
-	
+
 	//sizeof(rowbuf);
 
-	char colbuf[4];// = new unsigned char[4];
-	ZeroMemory(colbuf, 4);
+	char colbuf[5];// = new unsigned char[4];
+	ZeroMemory(colbuf, 5);
 	sprintf_s(colbuf, "%d", img0.cols);
 
-
-	for (size_t i = 0; i < 5; i++)
+	//add to sending bufer count of rows and cols
+	for (size_t i = 0; i < 6; i++)
 	{
 		ptrbuf[i] = rowbuf[i];
-		ptrbuf[i + 5] = colbuf[i];
+		ptrbuf[i + 6] = colbuf[i];
 	}
 
-
-	for (size_t i = 9; i <= img0.cols * img0.rows * 3 + 8; i++)
+	char lengthbuf[16];
+	ZeroMemory(lengthbuf, 16);
+	sprintf_s(lengthbuf, "%d", img0.cols * img0.rows * 3 + 26);
+	//add to sending bufer length of bytes of image.data
+	for (size_t i = 0; i < 17; i++)
 	{
-		ptrbuf[i] = img0.data[i - 9];
+		ptrbuf[i + 17] = lengthbuf[i];
 	}
-		
+	//add to sending buder image.data
+	for (size_t i = 27; i <= img0.cols * img0.rows * 3 + 26; i++)
+	{
+		ptrbuf[i] = img0.data[i - 27];
+	}
+	//testing: start
 	//cv::Mat img1 = cv::Mat(img0.rows, img0.cols, CV_8UC3);
 
 	//for (size_t i = 0; i <= img0.cols * img0.rows * 3; i++)
 	//{
-	//	img1.data[i] = ptrbuf[i + 9];
+	//	img1.data[i] = ptrbuf[i + 27];
 	//}
 	////img1.data = ptrbuf;
-	//cv::namedWindow("image", CV_WINDOW_NORMAL);
+	//cv::namedWindow("image", CV_WINDOW_AUTOSIZE);
 	//cv::imshow("image", img1);
 	//cv::waitKey(0);
-	
-
-
+	//testing: end
 	std::string ipAddress = "127.0.0.1";
 	int port = 54000;
 	//init winsocket
@@ -88,32 +94,39 @@ int main()
 		return -1;
 	}
 
-	//do-while send receive
-	char buf[4096];
-	std::string userinput;
-	do
+	const char* buf = ptrbuf;
+	int sendResult = send(sock, buf, img0.cols * img0.rows * 3 + 26, 0); //bufLen
+	if (sendResult != SOCKET_ERROR)
 	{
-		//detect some user text
-		std::cout << "> ";
-		std::getline(std::cin, userinput);
-		if (userinput.size() > 0)
-		{
-			//try send text
-			int sendResult = send(sock, userinput.c_str(), userinput.size() + 1, 0);
-			if (sendResult != SOCKET_ERROR)
-			{
-				//wait for response
-				ZeroMemory(buf, 4096);
-				int bytesReceived = recv(sock, buf, 4096, 0);
-				if (bytesReceived > 0)
-				{
-					//echo response to console
-					std::cout << "response from server: " << std::string(buf, 0, bytesReceived) << std::endl;
-				}
-			}
-		}
-		
-	} while (userinput.size() > 0);
+		std::cout << "bytes send : " << sendResult << std::endl;
+	}
+
+	//do-while send receive
+	//char buf[4096];
+	//std::string userinput;
+	//do
+	//{
+	//	//detect some user text
+	//	std::cout << "> ";
+	//	std::getline(std::cin, userinput);
+	//	if (userinput.size() > 0)
+	//	{
+	//		//try send text
+	//		int sendResult = send(sock, userinput.c_str(), userinput.size() + 1, 0);
+	//		if (sendResult != SOCKET_ERROR)
+	//		{
+	//			//wait for response
+	//			ZeroMemory(buf, 4096);
+	//			int bytesReceived = recv(sock, buf, 4096, 0);
+	//			if (bytesReceived > 0)
+	//			{
+	//				//echo response to console
+	//				std::cout << "response from server: " << std::string(buf, 0, bytesReceived) << std::endl;
+	//			}
+	//		}
+	//	}
+	//	
+	//} while (userinput.size() > 0);
 	//close and clean
 	closesocket(sock);
 	WSACleanup();
